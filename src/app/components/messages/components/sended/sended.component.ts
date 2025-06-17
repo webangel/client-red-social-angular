181dc87a3c7951a13835b0ef78b43a27ffe35953
Angel Torre C. <atorrec10@gmail.com>
2025-06-17 09:31:36 -0500

    se implemento la paginación de mensajería de enviados y recibidos, se corrigio la paginación de usuarios, se modifico un parámetro en las rutas de mensajería, se añadio iconos en el main de mansajería

--- src/app/components/messages/components/sended/sended.component.ts
+++ src/app/components/messages/components/sended/sended.component.ts
@@ -21,6 +21,11 @@ export class SendedComponent implements OnInit{
   public token;
   public url:string;
   public follows:any;
+  public page: number =0;
+  public next_page: number = 0;
+  public prev_page: number = 0;
+  public total: number = 0;
+  public pages: number = 0;
 
   constructor(
     private _route: ActivatedRoute,
@@ -36,14 +41,41 @@ export class SendedComponent implements OnInit{
   }
   ngOnInit(): void {
      console.log('main.component cargado!');
-     this.getMessage();
+     this.actualPage();
   }
-    getMessage(){
-    this._messageService.getEmmitMyMessages(this.token, 1).subscribe({
+  actualPage(){
+    this._route.params.subscribe(params =>{
+      let page = +params['page'];
+      this.page = Number(page);
+
+      if(!params['page']){
+        page = 1;
+      }
+      if(!page){
+        page = 1;
+      }else{
+        this.next_page = page +1;
+        this.prev_page = page -1;
+        if(this.prev_page <=0){
+          this.prev_page = 1;
+          console.log("prev_page", this.prev_page);
+        }
+      }
+
+      //devolver listado de usuarios
+      this.getMessage(this.token, this.page);
+    });
+  }
+    getMessage(token: any, page: number){
+    this._messageService.getEmmitMyMessages(token, page).subscribe({
       next: response => {
-        if(response.messages){
+        if(!response.messages){
+
+        }else{
           console.log("response.messages", response.messages);
           this.messages = response.messages;
+          this.total = response.total;
+          this.pages = response.pages
         }
       },
       error: err =>console.log(<any>err),
